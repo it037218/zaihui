ada0af98231fe90c9e11a8f71d42e964d47f653e
Your Name <you@example.com>
2018-05-02 21:18:19 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -38,3 +38,31 @@ $(function () {
 
     })
 })
+
+
+$(function(){
+    var showImgList = [
+        './static/images/swiper-1.png',
+        './static/images/swiper-2.png',
+        './static/images/swiper-3.png',
+        './static/images/swiper-4.png',
+        './static/images/swiper-5.png',
+    ];
+    let index = 0;
+    let liList = $('.cont-item');
+    setInterval(function(){
+        if(index >= liList.length - 1){
+            liList.eq(index).animate({left: '-20%', opacity: 0},600,function() {
+              liList.eq(0).animate({left: 0, opacity: 1}, 600);
+            });
+            index = 0;
+        }else{
+            liList.eq(index).animate({left: '-20%', opacity: 0},600,function() {
+              liList.eq(index + 1).animate({left: 0, opacity: 1}, 600);
+              index ++;
+            });
+        }
+        $(".swiper-show img").attr('src',showImgList[index])
+
+    },4000)
+});
